087f376a8c66fb947bc91236d5d56869a4e0bb82
Daniel Cvjeticanin <danicvj@gmail.com>
2020-09-03 11:07:56 +0200

    works

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("Hello World!");
+const express = require("express");
+
+const db = {
+  words: ["javascript", "python", "php"],
+};
+const app = express();
+
+// Configuration
+app.get("/hello", (request, response) => {
+  response.send("Hello");
+});
+
+app.get("/words/random", (request, response) => {
+  const randomIndex = Math.floor(Math.random() * db.words.length);
+  const word = db.words[randomIndex];
+  response.send({ word });
+});
+
+app.listen(3000, () => {
+  console.log("Server started on http://localhost:3000");
+});
